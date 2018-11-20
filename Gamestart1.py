a9a7b9b427d096e8cb97c171d0f022c871c1db26
Wyatt Nulton <legoman8304@gmail.com>
2018-11-19 19:39:07 -0500

    Trying to wrap whole game in an app class
    to see if it runs smoother?

--- Gamestart1.py
+++ Gamestart1.py
@@ -1,15 +1,15 @@
 #top
 import pyxel
-
-print("this is a test")
-pyxel.init(255, 255)
-
-def update():
-    if pyxel.btnp(pyxel.KEY_Q):
+class App:
+    pyxel.init(255, 255)
+    
+    
+    def update():
+        if pyxel.btnp(pyxel.KEY_Q):
         pyxel.quit()
 
-def draw():
-    pyxel.cls(0)
-    pyxel.rect(10, 10, 20, 20, 11)
+    def draw():
+        pyxel.cls(0)
+        pyxel.rect(10, 10, 20, 20, 11)
 
-pyxel.run(update, draw)
+App()
